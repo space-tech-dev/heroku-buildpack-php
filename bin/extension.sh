1d56e26d312f40d9aaa2c069094d15e7192c2dcf
MichaelHu <michael.hu@withinspace.com>
2019-08-26 12:02:44 +0800

    try extension.sh

--- bin/extension.sh
+++ bin/extension.sh
@@ -1,6 +1,34 @@
+# #!/bin/bash
+# # Build Path: /app/.heroku/php/
+# perl isntall swoole
+
 #!/bin/bash
 # Build Path: /app/.heroku/php/
-echo "ready to install swoole"
-pecl install swoole
+dep_url=https://github.com/swoole/swoole-src.git
+swoole_dir=swoole-src
+echo "-----> Building Swoole..."
 
-echo "finish to install swoole"
+### Phalcon
+echo "[LOG] Downloading SwoolePHP"
+git clone $dep_url -q
+if [ ! -d "$swoole_dir" ]; then
+  echo "[ERROR] Failed to find phalconphp directory $swoole_dir"
+  exit
+fi
+cd $swoole_dir/
+git checkout v4.3.6
+
+# /app/php/bin/phpize
+# ./configure --enable-phalcon --with-php-config=$PHP_ROOT/bin/php-config
+# make
+# make install
+BUILD_DIR=$1
+ln -s $BUILD_DIR/.heroku /app/.heroku
+export PATH=/app/.heroku/php/bin:$PATH
+bash phpize && \
+./configure && \
+make && make install
+
+# cd
+# echo "important extension phalcon into php.ini"
+# echo "extension=phalcon.so" >> /app/.heroku/php/etc/php/php.ini
